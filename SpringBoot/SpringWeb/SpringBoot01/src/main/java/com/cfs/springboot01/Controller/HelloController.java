3dde481972ad110ae78c1b37b88b070973c3d0de
Ayush Sharma <imayushdev.tech@gmail.com>
2026-09-23 20:42:19 +0530

    Add files via upload

---
+++ SpringBoot/SpringWeb/SpringBoot01/src/main/java/com/cfs/springboot01/Controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.cfs.springboot01.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello Spring Boot";
+    }
+
+    @GetMapping("/students")
+    public List<String> Student(){
+        return List.of("Ayush","Aryan");
+    }
+}
